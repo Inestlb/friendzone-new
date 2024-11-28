97eb6ff9c202465249050c20403cbba18486aaca
tatiana villamizar <tatianavizar@gmail.com>
2024-11-28 17:46:47 +0100

    Modal after a match

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -4,12 +4,14 @@ class LikesController < ApplicationController
     # @user = User.find(params[:user_id])
     @like = Like.new(
       liker: current_user,
-      liked: User.find(params[:liked])
+      liked: User.find(params[:liked]),
+      is_liked: params[:is_liked]
     )
 
     if @like.save
+      match = check_for_match(@like) if @like.is_liked
       respond_to do |format|
-        format.json { render(json: { content: "Youpi" }) }
+        format.json { render(json: { content: "YOUPI", match: true, message: "You matched with #{@like.liked.name}!" }) }
         format.html { redirect_to users_path, status: :see_other, notice: "You matched with #{@like.liked.name}!" }
       end
     else
@@ -21,5 +23,14 @@ class LikesController < ApplicationController
 
   end
 
+  private
+
+  def check_for_match(like)
+    mate = User.find(like.liked_id)
+    if Like.find_by(liker: mate, liked: current_user, is_liked: true)
+      Match.create(first_user: mate, second_user: current_user)
+
+    end
+  end
 
 end
