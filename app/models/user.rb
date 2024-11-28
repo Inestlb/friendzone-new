a7d72fe49418a10be6849c90f0a7e6a505d1fa7f
Lea Alcou <leaalcou@gmail.com>
2024-11-28 14:45:13 +0100

    changes

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,15 +7,15 @@ class User < ApplicationRecord
   has_one_attached :avatar
   has_many_attached :photos
 
-  has_many :messages
+  has_many :messages, dependent: :destroy
 
-  has_many :matches_as_first_user, class_name: 'Match', foreign_key: 'first_user_id'
-  has_many :matches_as_second_user, class_name: 'Match', foreign_key: 'second_user_id'
+  has_many :matches_as_first_user, class_name: 'Match', foreign_key: 'first_user_id', dependent: :destroy
+  has_many :matches_as_second_user, class_name: 'Match', foreign_key: 'second_user_id', dependent: :destroy
 
-  has_many :likes_as_liker, class_name: 'Like', foreign_key: 'liker_id'
-  has_many :likes_as_liked, class_name: 'Like', foreign_key: 'liked_id'
+  has_many :likes_as_liker, class_name: 'Like', foreign_key: 'liker_id', dependent: :destroy
+  has_many :likes_as_liked, class_name: 'Like', foreign_key: 'liked_id', dependent: :destroy
 
-  has_many :user_events
+  has_many :user_events, dependent: :destroy
   has_many :events, through: :user_events
 
   # validates :age, presence: true, comparison: { greater_than_or_equal_to: 18 }
@@ -35,6 +35,4 @@ class User < ApplicationRecord
   def all_matches
     matches_as_first_user + matches_as_second_user
   end
-
-
 end
