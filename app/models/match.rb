a5add4487cfd69dcb3ac7f5075f8456d61f17e37
ines talbi <ines-talbi@hotmail.fr>
2024-11-29 12:01:43 +0100

    add method to match

--- app/models/match.rb
+++ app/models/match.rb
@@ -1,8 +1,13 @@
 class Match < ApplicationRecord
   belongs_to :first_user, class_name: "User"
   belongs_to :second_user, class_name: "User"
+  has_many :messages
 
   def matched_user(current_user)
     first_user_id == current_user.id ? second_user : first_user
+  end
+
+  def last_message
+    messages.last
   end
 end
